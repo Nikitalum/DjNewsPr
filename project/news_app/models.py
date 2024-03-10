029799c2ed8951b59f439122b202ac972f0c5d1b
Nikita <chesterryos@gmail.com>
2024-03-10 11:32:03 +0900

    Создан urls.py в приложении news_app, добавлены представления во views.py, добавлен список новостей, добавлена таблица вывода списка и заголовков

--- project/news_app/models.py
+++ project/news_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Articles(models.Model):
+    title = models.CharField('Название', max_length=50)
+    text = models.TextField('Текст')
+    date = models.DateTimeField('Дата')
+
+    def __str__(self):
+        return self.title
--- project/news_app/views.py
+++ project/news_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    return render(request, 'flatpages/default.html')
+
+def news(request):
+    data = {
+        'title': 'Новости',
+        'values': ['Новость 1','Новость 2','Новость 3','Новость 4','Новость 5']
+    }
+    return render(request, 'flatpages/news.html', data)
+
+def about(request):
+    return HttpResponse('<h2>О сайте</h2>')
+
+def details(request):
+    return render(request, 'flatpages/about.html')
